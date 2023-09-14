aa251c0db3bc25b86ea0aa9fa344a93728f80900
Alimur Razi <138320899+alimurdipu@users.noreply.github.com>
2023-09-14 20:53:04 +0600

    424

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,11 +1,22 @@
 import PropTypes from 'prop-types';
 const Blog = ({blog}) => {
-console.log(blog)
+    const {title, cover, author, author_img, reading_time, posted_date} = blog;
     return (
         <div>
-            {
-                blog.map(blog => <h2>{blog.title}</h2>)
-            }
+            <img src={cover} alt={`Cover picture of the title ${title}`} />
+            <div className='flex justify-between'>
+                <div className='flex'>
+                    <img className='w-14' src={author_img} alt="" />
+                    <div className='ml-6'>
+                        <h3 className='text-2xl'>{author}</h3>
+                        <p>{posted_date}</p>
+                    </div>
+                </div>
+                <div>
+                    <span>{reading_time} min read</span>
+                </div>
+            </div>
+            <h2 className="text-4xl">{title}</h2>
         </div>
     );
 };
